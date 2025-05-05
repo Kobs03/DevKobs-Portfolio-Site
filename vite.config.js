a5dabc4cb29efb3bb5d3ec9f8d5aecbba0ebec75
Jake S. Dedicatoria <thisisjake03@gmail.com>
2025-05-06 05:04:53 +0800

    changed github base in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DevKobs-Portfolio-Web/',
+  base: '/DevKobs-Portfolio-Site/',
   plugins: [
     vue(),
     vueDevTools(),
